34bcd20e31bf9e0c6a34d89cd58566d67a98c089
Alessia Vitale <alessiavitale1996@gmail.com>
2018-05-06 17:19:51 +0200

    sf

--- SenateResponse.java
+++ SenateResponse.java
@@ -24,31 +24,31 @@ public class SenateResponse {
         this.metadata = new HashMap<String,Object>();
         this.results = new ArrayList<Result>();
     }
-
+/** Comments about this class */
     public void setResults(ArrayList<Result> results) {
         this.results = results;
     }
-
+/** Comments about this class */
     public ArrayList<Result> getResults() {
         return this.results;
     }
-
+/** Comments about this class */
     public void addResult(Result result) {
         this.results.add(result);
     }
-
+/** Comments about this class */
     public HashMap<String, Object> getMetadata() {
         return this.metadata;
     }
-
+/** Comments about this class */
     public void setMetadata(HashMap<String, Object> metadata) {
         this.metadata = metadata;
     }
-
+/** Comments about this class */
     public void addMetadataByKey(String key, Object value) {
         this.metadata.put(key, value);
     }
-
+/** Comments about this class */
     public Object getMetadataByKey(String key) {
         return this.metadata.get(key);
     }
